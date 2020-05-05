ff1db522321c4094a673f31e3db29984514336d0
Donavan Brown <dkb@Donavans-MacBook-Pro.local>
2020-05-05 18:29:47 -0400

    make it better

--- src/components/projects.js
+++ src/components/projects.js
@@ -31,7 +31,9 @@ class Projects extends Component {
                 color: "black",
                 height: "176px",
                 background:
-                  "https://dkb715.github.io/MyResume/img/burger.png",
+                "url(https://dkb715.github.io/MyResume/img/carproject.png) center / cover",
+
+                  
               }}
             >
               Philly Auto Imporium
@@ -83,7 +85,7 @@ class Projects extends Component {
               <Button>
                 <a
                   style={{ textDecoration: "none" }}
-                  href="https://github.com/Dioris-cpu/noteTaker"
+                  href="https://github.com/dkb715/notetaker2"
                 >
                   {" "}
                   GitHub
@@ -92,7 +94,7 @@ class Projects extends Component {
               <Button>
                 <a
                   style={{ textDecoration: "none" }}
-                  href="https://takerdioris.herokuapp.com"
+                  href=" https://notetaker7.herokuapp.com/"
                 >
                   Live Demo
                 </a>
@@ -109,20 +111,20 @@ class Projects extends Component {
                 color: "black",
                 height: "176px",
                 background:
-                  "url(https://dioris-cpu.github.io/Homework-9/img/Screen%20Shot%202020-03-19%20at%2011.49.20%20AM.png) center / cover",
+                  "url(https://dkb715.github.io/MyResume/img/burger.png) center / cover",
               }}
             >
               EatDaBurger
             </CardTitle>
             <CardText>
               This application allows the user to create his or her own burger
-              and and gives them the options to devour it.
+              and can choose to "eat" it 
             </CardText>
             <CardActions border>
               <Button>
                 <a
                   style={{ textDecoration: "none" }}
-                  href="https://github.com/Dioris-cpu/burgers"
+                  href="https://github.com/dkb715/mvc"
                 >
                   {" "}
                   GitHub
@@ -131,7 +133,119 @@ class Projects extends Component {
               <Button>
                 <a
                   style={{ textDecoration: "none" }}
-                  href="https://intense-chamber-29665.herokuapp.com/"
+                  href="https://mvc7.herokuapp.com/"
+                >
+                  Live Demo
+                </a>
+              </Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "176px",
+                background:
+                  "url(https://dkb715.github.io/MyResume/img/workday.jpg) center / cover",
+              }}
+            >
+              Work Day Scheduler
+            </CardTitle>
+            <CardText>
+              This application allows the user to create his or her own schedule between 9 and 5
+            </CardText>
+            <CardActions border>
+              <Button>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://dkb715.github.io/5HW/"
+                >
+                  {" "}
+                  GitHub
+                </a>
+              </Button>
+              <Button>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://github.com/dkb715/5HW"
+                >
+                  Live Demo
+                </a>
+              </Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "176px",
+                background:
+                  "url(https://dkb715.github.io/MyResume/img/teamsummary.jpg) center / cover",
+              }}
+            >
+              Team Summary
+            </CardTitle>
+            <CardText>
+             Here the user can create a page to track his or her team.
+            </CardText>
+            <CardActions border>
+              <Button>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://dkb715.github.io/summary4/"
+                >
+                  {" "}
+                  GitHub
+                </a>
+              </Button>
+              <Button>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://github.com/dkb715/summary4"
+                >
+                  Live Demo
+                </a>
+              </Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "176px",
+                background:
+                  "url(https://user-images.githubusercontent.com/48693333/72358079-01c92d80-36ba-11ea-8ac0-06fa6704a6cd.png) center / cover",
+              }}
+            >
+              WishDish!
+            </CardTitle>
+            <CardText>
+              This application is for indecisive users who need to decide on what to eat fast!
+            </CardText>
+            <CardActions border>
+              <Button>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://dioris-cpu.github.io/group-Project-1/"
+                >
+                  {" "}
+                  GitHub
+                </a>
+              </Button>
+              <Button>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://github.com/Dioris-cpu/group-Project-1"
                 >
                   Live Demo
                 </a>
